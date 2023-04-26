f098b47e24bab777533af83a493cc3cc8eab88a4
LaneyBlack-Laptop <laney.black.74@gmail.com>
2023-04-26 14:14:42 +0200

    C6 done

--- C6/Program.cs
+++ C6/Program.cs
@@ -14,7 +14,7 @@ namespace C6
         public static void Main()
         {
             ClearFileCsv();
-            var spring = new Spring(Math.PI/2, 10, Dimensions);
+            var spring = new Spring(Math.PI / 2, 10, Dimensions);
             SetHeaderCsv(spring);
             double time = 0;
             const double dT = 0.3;
@@ -23,16 +23,27 @@ namespace C6
                 ExportCsv(time, spring);
                 spring.CountNextMidPoint(dT);
                 time += dT;
-            } while (time<=6);
+            } while (time <= 6);
         }
 
         private static void ExportCsv(double time, Spring spring)
         {
             File.AppendAllText(FilePath, $"{time},");
+            double ep = 0, ek = 0;
+            for (var i = 1; i < spring.Points.Length; i++)
+            {
+                ep += Math.Pow(spring.Points[i].S[1] - spring.Points[i - 1].S[1], 2);
+                ek += Math.Pow(spring.Points[i].Vy, 2);
+            }
+
+            ep *= 1 / (2 * spring.Dx);
+            ek *= spring.Dx / 2;
             foreach (var point in spring.Points)
             {
-                File.AppendAllText(FilePath, $"{Math.Round(point.S[1],3)},");
+                File.AppendAllText(FilePath, $"{Math.Round(point.S[1], 3)},");
             }
+
+            File.AppendAllText(FilePath, $",{Math.Round(ep, 3)},{Math.Round(ek, 3)},{Math.Round(ep + ek, 3)}");
             File.AppendAllText(FilePath, "\n");
         }
 
@@ -46,6 +57,7 @@ namespace C6
             File.AppendAllText(FilePath, "Time,");
             for (var i = 0; i < spring.Points.Length; i++)
                 File.AppendAllText(FilePath, $"{i},");
+            File.AppendAllText(FilePath, ",Ep,Ek,Ec");
             File.AppendAllText(FilePath, "\n");
         }
     }
