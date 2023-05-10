09ec234f370f8579df32f3e78fea204740702f1f
LaneyBlack-Laptop <laney.black.74@gmail.com>
2023-05-11 00:33:03 +0200

    Template for C9 done.

---
+++ C9/PhysicalObject.cs
@@ -0,0 +1,60 @@
+﻿namespace C9
+{
+    public class PhysicalObject
+    {
+        public double[] S { get; set; } //0-X, 1-Y, 2-Z ...
+        public double[] V { get; set; }
+        public double[] A { get; set; }
+
+        public PhysicalObject(int dimensions)
+        {
+            S = new double[dimensions];
+        }
+
+        public void CalcNext(double dT)
+        {
+            for (int i = 0; i < S.Length; i++)
+            {
+                S[i] += V[i] * dT; // S(t) = S(0) + V(0) * dT
+                V[i] += A[i] * dT;
+                A[i] = 9.81;
+            }
+        }
+
+        public void CalcNextMidPoint(double dT)
+        {
+            for (int i = 0; i < S.Length; i++)
+            {
+                var halfV = A[i] * dT / 2;
+                var halfA = 9.81;
+
+                var dS = halfV * dT;
+                var dV = halfA * dT;
+
+                S[i] += dS;
+                V[i] += dV;
+                A[i] = 9.81;
+            }
+        }
+
+        public void CalcNextRk4(double dT)
+        {
+            //ToDo get this done
+            var k = new double[4];
+            double dTn = 0, sum = 0;
+            for (int i = 0; i < 4; i++)
+            {
+                if (i != 2)
+                    dTn += (dT / 2);
+                var tN = i * dT;
+                k[i] = dTn * V[i];
+                if (i == 1 || i == 2)
+                    sum += 2 * k[i];
+                sum += k[i];
+            }
+            sum /= 6;
+            sum *= dT;
+            S[0] += sum;
+        }
+    }
+}
---
+++ C9/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.IO;
+
+namespace C9
+{
+    internal class Program
+    {
+        private const string FilePath = //"C:\\PJATK\\4th\\PSM\\C7\\Data\\exp.csv";
+            "C:\\PJATK\\4th\\PSM\\CwiczeniaPSM\\C9\\Data\\exp.csv";
+
+        private const int Dimensions = 2;
+
+        public static void Main()
+        {
+            ClearFileCsv();
+            
+            SetHeaderCsv();
+            double time = 0;
+            const double dT = 0.1;
+            var physObj = new PhysicalObject(Dimensions);
+            do
+            {
+                ExportToCsv(time, physObj);
+                physObj.CalcNext(dT);
+                time += dT;
+            } while (time <= 6);
+
+            SetHeaderCsv();
+            time = 0;
+            physObj = new PhysicalObject(Dimensions);
+            do
+            {
+                ExportToCsv(time, physObj);
+                physObj.CalcNextMidPoint(dT);
+                time += dT;
+            } while (time <= 6);
+
+            SetHeaderCsv();
+            time = 0;
+            physObj = new PhysicalObject(Dimensions);
+            do
+            {
+                ExportToCsv(time, physObj);
+                physObj.CalcNextRk4(dT);
+                time += dT;
+            } while (time <= 6);
+        }
+
+        private static void ClearFileCsv()
+        {
+            File.WriteAllText(FilePath, "");
+        }
+
+        private static void SetHeaderCsv()
+        {
+            File.AppendAllText(FilePath, "\n\n\n");
+            File.AppendAllText(FilePath,"T,Sx,Sy");
+        }
+
+        private static void ExportToCsv(double time, PhysicalObject physObj)
+        {
+            File.AppendAllText(FilePath, "\n\n");
+            File.AppendAllText(FilePath, $"{Math.Round(time, 3)},");
+            foreach (var s in physObj.S)
+                File.AppendAllText(FilePath, $"{Math.Round(s, 3)},");
+
+            File.AppendAllText(FilePath, "\n");
+        }
+    }
+}
