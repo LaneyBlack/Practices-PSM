3fbb4a89ebb949143190ae8b423acba8cf97ef8b
LaneyBlack-Laptop <laney.black.74@gmail.com>
2023-05-11 18:26:32 +0200

    C9 done

---
+++ C9/ButterFly.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace C9
+{
+    public class ButterFly
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Z { get; set; }
+        public double A { get; set; }
+        public double B { get; set; }
+        public double C { get; set; }
+
+        public ButterFly(double s0, double a, double b, double c)
+        {
+            X = s0;
+            Y = s0;
+            Z = s0;
+            A = a;
+            B = b;
+            C = c;
+        }
+
+        public void CalcNext(double dT)
+        {
+            double dX = (A * Y - A * X) * dT; // dX = (A*y - A*x) * dT
+            double dY = (-X * Z + B * X - Y) * dT; // dY = (-x*z + B*x - y) * dT
+            double dZ = (X * Y - C * Z) * dT; // dZ = (x*y - C*z) * dT
+            X += dX;
+            Y += dY;
+            Z += dZ;
+        }
+
+        public void CalcNextMidPoint(double dT)
+        {
+            var halfX = X + (A * Y - A * X) * dT / 2; // in middle point
+            var halfY = Y + (-X * Z + B * X - Y) * dT / 2;
+            var halfZ = Z + (X * Y - C * Z) * dT / 2;
+
+            X += (A * halfY - A * halfX) * dT;
+            Y += (-halfX * halfZ + B * halfX - halfY) * dT;
+            Z += (halfX * halfY - C * halfZ) * dT;
+        }
+
+        public void CalcNextRk4(double dT)
+        {
+            var k = new double[4][];
+            k[0] = CalcDerivative(X, Y, Z);
+            for (int i = 1; i < 4; i++)
+            {
+                var next = i == 3
+                    ? CountNext(dT, k[i - 1][0], k[i - 1][1], k[i - 1][2])
+                    : CountNext(dT / 2, k[i - 1][0], k[i - 1][1], k[i - 1][2]);
+                k[i] = CalcDerivative(next[0], next[1], next[2]);
+            }
+
+            // Count sum's
+            var kSum = new double[3];
+            for (int i = 0; i < 4; i++)
+            {
+                int tmp = 1;
+                if (i == 1 || i == 2)
+                    tmp = 2;
+                for (int j = 0; j < 3; j++)
+                {
+                    kSum[j] += k[i][j] * tmp;
+                }
+            }
+            X += kSum[0] / 6 * dT;
+            Y += kSum[1] / 6 * dT;
+            Z += kSum[2] / 6 * dT;
+        }
+
+        private double[] CalcDerivative(double x, double y, double z)
+        {
+            var derX = (A * y - A * x);
+            var derY = -x * z + B * x - y;
+            var derZ = x * y - C * z;
+            return new[] { derX, derY, derZ };
+        }
+
+        private double[] CountNext(double dT, double kx, double ky, double kz)
+        {
+            var nextX = X + kx * dT;
+            var nextY = Y + ky * dT;
+            var nextZ = Z + kz * dT;
+            return new[] { nextX, nextY, nextZ };
+        }
+    }
+}
--- C9/Program.cs
+++ C9/Program.cs
@@ -8,42 +8,42 @@ namespace C9
         private const string FilePath = //"C:\\PJATK\\4th\\PSM\\C7\\Data\\exp.csv";
             "C:\\PJATK\\4th\\PSM\\CwiczeniaPSM\\C9\\Data\\exp.csv";
 
-        private const int Dimensions = 2;
-
         public static void Main()
         {
+            const double a = 10, b = 25, c = 8.0 / 3.0, s0 = 1;
             ClearFileCsv();
-            
-            SetHeaderCsv();
-            double time = 0;
-            const double dT = 0.1;
-            var physObj = new PhysicalObject(Dimensions);
-            do
-            {
-                ExportToCsv(time, physObj);
-                physObj.CalcNext(dT);
-                time += dT;
-            } while (time <= 6);
 
             SetHeaderCsv();
-            time = 0;
-            physObj = new PhysicalObject(Dimensions);
-            do
+            int iterations = 0;
+            double dT = 0.03;
+            var butterFly = new ButterFly(s0,a,b,c);
+            while (iterations < 1500)
             {
-                ExportToCsv(time, physObj);
-                physObj.CalcNextMidPoint(dT);
-                time += dT;
-            } while (time <= 6);
+                ExportToCsv(iterations, butterFly);
+                butterFly.CalcNext(dT);
+                iterations++;
+            }
 
             SetHeaderCsv();
-            time = 0;
-            physObj = new PhysicalObject(Dimensions);
-            do
+            iterations = 0;
+            dT = 0.03;
+            butterFly = new ButterFly(s0,a,b,c);
+            while (iterations < 1500)
             {
-                ExportToCsv(time, physObj);
-                physObj.CalcNextRk4(dT);
-                time += dT;
-            } while (time <= 6);
+                ExportToCsv(iterations, butterFly);
+                butterFly.CalcNextMidPoint(dT);
+                iterations++;
+            }
+
+            SetHeaderCsv();
+            iterations = 0;
+            butterFly = new ButterFly(s0,a,b,c);
+            while (iterations < 1500)
+            {
+                ExportToCsv(iterations, butterFly);
+                butterFly.CalcNextRk4(dT);
+                iterations ++;
+            }
         }
 
         private static void ClearFileCsv()
@@ -54,16 +54,14 @@ namespace C9
         private static void SetHeaderCsv()
         {
             File.AppendAllText(FilePath, "\n\n\n");
-            File.AppendAllText(FilePath,"T,Sx,Sy");
+            File.AppendAllText(FilePath, "T,X,Z,Y");
+            File.AppendAllText(FilePath, "\n");
         }
 
-        private static void ExportToCsv(double time, PhysicalObject physObj)
+        private static void ExportToCsv(double time, ButterFly butterFly)
         {
-            File.AppendAllText(FilePath, "\n\n");
-            File.AppendAllText(FilePath, $"{Math.Round(time, 3)},");
-            foreach (var s in physObj.S)
-                File.AppendAllText(FilePath, $"{Math.Round(s, 3)},");
-
+            File.AppendAllText(FilePath, $"{Math.Round(time, 3)},{Math.Round(butterFly.X,3)}," +
+                                         $"{Math.Round(butterFly.Z,3)},{Math.Round(butterFly.Y,3)},");
             File.AppendAllText(FilePath, "\n");
         }
     }
