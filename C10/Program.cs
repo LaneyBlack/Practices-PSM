55f7f6b916d659886ab47788c3c450c78f57fc65
LaneyBlack-Laptop <laney.black.74@gmail.com>
2023-05-25 21:27:23 +0200

    Fractal Plant Simulator finished

--- C10/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace C10
-{
-    internal class Program
-    {
-        public static void Main(string[] args)
-        {
-        }
-    }
-}
--- FractalPlantGenerator/main.py
+++ FractalPlantGenerator/main.py
@@ -6,6 +6,7 @@ x_formula = "F+[[X]-X]-F[-FX]+X"  # X -> F+[[X]-X]-F[-FX]+X
 f_formula = "FF"  # F -> FF
 
 
+# definition of screen
 class Screen:
     def __init__(self):
         # Init window
@@ -16,15 +17,15 @@ class Screen:
         self.brush = turtle.Turtle()
         self.brush.color("green")
         self.brush.shape("classic")
+        self.brush.shapesize(stretch_wid=0.5)
         self.brush.speed(0)
-        # self.brush.shapesize(stretch_wid=0.5)
         # Put brush into start pos
         self.brush.penup()
-        self.brush.goto(0, -self.window.window_height() / 2)
+        self.brush.goto(0, -self.window.window_height() / 2) # screen center
         self.brush.left(90)  # turn upside
         self.brush.pendown()
 
-
+# convert instructions
 def get_instructions(instructions, x_formula, f_formula, n):
     for i in range(n):
         instructions = instructions.replace("F", f_formula)
@@ -35,23 +36,23 @@ def get_instructions(instructions, x_formula, f_formula, n):
 def draw_instructions(instructions, screen):
     stack = []
     for character in instructions:
-        if character == 'F':
+        if character == 'F':  # then draw
             screen.brush.forward(10)
-        elif character == '+':
+        elif character == '+':  # then turn left
             screen.brush.left(25)
-        elif character == '-':
+        elif character == '-':  # then turn right
             screen.brush.right(25)
-        elif character == '[':
+        elif character == '[':  # add to que last coordinate
             coordinates = [screen.brush.xcor(), screen.brush.ycor()]
             angle = screen.brush.heading()
             stack.append((angle, coordinates))
-        elif character == ']':
+        elif character == ']':  # pop from queue
             angle, coordinates = stack.pop()
             screen.brush.penup()
             screen.brush.setheading(angle)
             screen.brush.goto(coordinates[0], coordinates[1])
             screen.brush.pendown()
-        elif character == 'X':
+        elif character == 'X':  # do nothing
             screen.brush.forward(0)  # 0 or 25
     screen.window.exitonclick()
 
